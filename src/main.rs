788239f6862b59f66df0cc7cdfd1d9b013cf33c2
Jonathan Woollett-Light <jonathanwoollettlight@gmail.com>
2024-01-08 18:28:23 +0000

    step

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("Hello, world!");
     println!("I'm going for a walk");
-    println!("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    println!("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     println!("Goodbye, world!");
 }
