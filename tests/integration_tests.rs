1861ec9b0f57f5788eed7b04c96290b22fc1131e
Jonathan Woollett-Light <jonathanwoollettlight@gmail.com>
2024-01-08 18:07:24 +0000

    Improve metric gathering

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,116 +1,103 @@
 use rand::Rng;
 use std::collections::HashMap;
-use std::io::Write;
-use std::sync::Arc;
 use std::sync::Mutex;
 use std::sync::OnceLock;
 
-static HANDLER: OnceLock<Arc<Mutex<MetricFileHandle>>> = OnceLock::new();
+static METRICS: Metrics = Metrics::new();
 
-struct MetricFileHandle {
-    target: &'static str,
-    current: HashMap<&'static str, u64>,
-}
-impl MetricFileHandle {
-    fn add(&mut self, name: &'static str, value: u64) {
-        self.current.insert(name, value);
+struct Metrics(OnceLock<MetricsInner>);
+struct MetricsInner(Mutex<(u64, HashMap<&'static str, u64>)>);
+struct MetricsHandle<'a>(&'a MetricsInner);
+
+impl Metrics {
+    pub const fn new() -> Self {
+        Self(OnceLock::new())
     }
-    fn flush(&self) {
-        let mut file = std::fs::OpenOptions::new()
-            .create(true)
-            .truncate(true)
-            .write(true)
-            .open(&self.target)
-            .unwrap();
+    fn handle(&self) -> MetricsHandle<'_> {
+        let metrics = self
+            .0
+            .get_or_init(|| MetricsInner(Mutex::new((0, HashMap::new()))));
+        let mut guard = metrics.0.lock().unwrap();
+        let (count, _map) = &mut *guard;
+        *count += 1;
 
-        let mut iter = self.current.iter().peekable();
-        writeln!(&mut file, "{{").unwrap();
-        while let Some((name, value)) = iter.next() {
-            writeln!(
-                &mut file,
-                "    \"{name}\": {value}{}",
-                if iter.peek().is_some() { "," } else { "" }
-            )
-            .unwrap()
+        MetricsHandle(metrics)
+    }
+}
+impl MetricsHandle<'_> {
+    fn add(&self, name: &'static str, value: u64) {
+        let mut guard = self.0 .0.lock().unwrap();
+        let (_count, map) = &mut *guard;
+        map.insert(name, value);
+    }
+}
+impl Drop for MetricsHandle<'_> {
+    fn drop(&mut self) {
+        let mut guard = self.0 .0.lock().unwrap();
+        let (count, map) = &mut *guard;
+        *count -= 1;
+        if *count == 0 {
+            let file = std::fs::OpenOptions::new()
+                .create(true)
+                .truncate(true)
+                .write(true)
+                .open("metrics")
+                .unwrap();
+            serde_json::to_writer_pretty(file, map).unwrap();
         }
-        writeln!(&mut file, "}}").unwrap();
     }
-}
-
-fn init<'a>() -> &'a Arc<Mutex<MetricFileHandle>> {
-    HANDLER.get_or_init(|| {
-        Arc::new(Mutex::new(MetricFileHandle {
-            target: "./metrics",
-            current: HashMap::new(),
-        }))
-    })
-}
-
-fn flush() {
-    let reference = init();
-    reference.lock().unwrap().flush();
-}
-
-fn add(name: &'static str, value: u64) {
-    let reference = init();
-    reference.lock().unwrap().add(name, value);
 }
 
 // Upload 1.
 #[test]
 fn constant() {
-    add("constant", 1);
-
-    flush();
+    let handle = METRICS.handle();
+    handle.add("constant", 1);
 }
 
 // Upload a random integer between 0 and 2^8.
 #[test]
 fn random_one() {
-    add("random_one", rand::thread_rng().gen::<u8>() as u64);
-
-    flush();
+    let handle = METRICS.handle();
+    handle.add("random_one", rand::thread_rng().gen::<u8>() as u64);
 }
 
 // Upload a random integer between 0 and 2^16.
 #[test]
 fn random_two() {
-    add("random_two", rand::thread_rng().gen::<u16>() as u64);
-
-    flush();
+    let handle = METRICS.handle();
+    handle.add("random_two", rand::thread_rng().gen::<u16>() as u64);
 }
 
 // 1/4 of the time doesn't upload metrics.
 #[test]
 fn inconsistent() {
+    let handle = METRICS.handle();
     if rand::thread_rng().gen::<u8>() > (u8::MAX / 4) {
-        add("inconsistent_constant", 2);
-        add("inconsistent_random", rand::thread_rng().gen::<u8>() as u64);
+        handle.add("inconsistent_constant", 2);
+        handle.add("inconsistent_random", rand::thread_rng().gen::<u8>() as u64);
     }
-
-    flush();
 }
 
 #[test]
 fn many() {
+    let handle = METRICS.handle();
     let mut rng = rand::thread_rng();
-    add("random_u8_one", rng.gen::<u8>() as u64);
-    add("random_u8_two", rng.gen::<u8>() as u64);
-    add("random_u8_three", rng.gen::<u8>() as u64);
-    add("random_u16_one", rng.gen::<u16>() as u64);
-    add("random_u16_two", rng.gen::<u16>() as u64);
-    add("random_u16_three", rng.gen::<u16>() as u64);
-    add("random_u32_one", rng.gen::<u32>() as u64);
-    add("random_u32_two", rng.gen::<u32>() as u64);
-    add("random_u32_three", rng.gen::<u32>() as u64);
+    handle.add("random_u8_one", rng.gen::<u8>() as u64);
+    handle.add("random_u8_two", rng.gen::<u8>() as u64);
+    handle.add("random_u8_three", rng.gen::<u8>() as u64);
+    handle.add("random_u16_one", rng.gen::<u16>() as u64);
+    handle.add("random_u16_two", rng.gen::<u16>() as u64);
+    handle.add("random_u16_three", rng.gen::<u16>() as u64);
+    handle.add("random_u32_one", rng.gen::<u32>() as u64);
+    handle.add("random_u32_two", rng.gen::<u32>() as u64);
+    handle.add("random_u32_three", rng.gen::<u32>() as u64);
 
-    add("random_many_one", rng.gen::<u16>() as u64);
-    add("random_many_two", rng.gen::<u16>() as u64);
-    add("random_many_three", rng.gen::<u16>() as u64);
-    add("random_many_four", rng.gen::<u16>() as u64);
-    add("random_many_five", rng.gen::<u16>() as u64);
-    add("random_many_six", rng.gen::<u16>() as u64);
-    add("random_many_seven", rng.gen::<u16>() as u64);
-
-    flush();
+    handle.add("random_many_one", rng.gen::<u16>() as u64);
+    handle.add("random_many_two", rng.gen::<u16>() as u64);
+    handle.add("random_many_three", rng.gen::<u16>() as u64);
+    handle.add("random_many_four", rng.gen::<u16>() as u64);
+    handle.add("random_many_five", rng.gen::<u16>() as u64);
+    handle.add("random_many_six", rng.gen::<u16>() as u64);
+    handle.add("random_many_seven", rng.gen::<u16>() as u64);
 }
